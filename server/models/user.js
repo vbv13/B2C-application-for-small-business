c97278d06387069d045f4fce0904b6c91e5fe1b9
vbv13 <pjastrzebscy@gmail.com>
2019-07-05 14:40:58 +0200

    server: login users and creating tokens

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
         email:{
@@ -57,8 +59,26 @@ userSchema.pre('save', function(next){
     } else {
         next();
     }
-
 })
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)   //null as first argument, cause we don't get err from bcrypt.compare, comparison hitted so isMatch is true
+    })
+}
+
+userSchema.methods.generateToken = (cb) => {
+    //console.log(this)  //should be a user, if not create a variable with this pointing to a user
+    let token = jwt.sign(this._id.toHexString(), process.env.SECRET);
+
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    })
+
+}
 
 const User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -33,6 +33,26 @@ app.post('/api/users/register', (req, res) => {
     })
 });
 
+app.post('/api/users/login', (req, res) => {
+        //find the email
+        //check password
+        //generate a token (if password is correct for specific email)
+    User.findOne({'email':req.body.email}, (err,user) => {
+        if(!user) return res.json({loginSuccess: false, message: 'Authorization failed, we did not found such an email'});
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({loginSuccess: false, message: 'Wrong password'});
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
+        })
+    })
+})
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, ()=>{

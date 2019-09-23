ce3fb272bf538cfe557ca29f17194862dff9fec1
piotrek <pjastrzebscy@gmail.com>
2019-09-23 14:53:34 +0200

    cart component

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -3,7 +3,9 @@ import {
     REGISTER_USER,
     AUTH_USER,
     LOGOUT_USER,
-    ADD_TO_CART_USER
+    ADD_TO_CART_USER,
+    GET_CART_ITEMS_USER,
+    REMOVE_CART_ITEM_USER
 } from '../actions/types';
 
 export default function(state={}, action) {
@@ -22,7 +24,18 @@ export default function(state={}, action) {
                     ...state.userData,
                     cart: action.payload
                 }
-            }           
+            }       
+        case GET_CART_ITEMS_USER:
+            return {...state, cartDetail: action.payload}
+        case REMOVE_CART_ITEM_USER:
+            return {
+                ...state,
+                cartDetail: action.payload.cartDetail,
+                userData:{
+                    ...state.userData,
+                    cart: action.payload.cart
+                }
+            }    
         default:
             return state;
     }
